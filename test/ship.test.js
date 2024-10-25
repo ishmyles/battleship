dbfdc62332605a130e8e7a8e9d793b20b786d7eb
Myles Gaerlan <ish.myles@gmail.com>
2024-10-26 02:11:55 +1100

    test: update ship tests
    Group unit tests by ship types for organisation

--- test/ship.test.js
+++ test/ship.test.js
@@ -17,129 +17,86 @@ test("creates new ship", () => {
 });
 
 // Destroyer ship
-test("destroyer is hit and is not yet sunk", () => {
+describe("destroyer ship", () => {
   const destroyer = Ship("destroyer");
 
-  expect(destroyer.hit()).toBe(false);
-});
+  test("is hit and is not yet sunk", () => {
+    expect(destroyer.hit()).toBe(false);
+  });
 
-test("destroyer is hit 2 times and is sunk", () => {
-  const destroyer = Ship("destroyer");
-  destroyer.hit();
-
-  expect(destroyer.hit()).toBe(true);
+  test("is hit 2 times and is sunk", () => {
+    expect(destroyer.hit()).toBe(true);
+  });
 });
 
 // Submarine ship
-test("submarine is hit and is not yet sunk", () => {
+describe("submarine ship", () => {
   const submarine = Ship("submarine");
 
-  expect(submarine.hit()).toBe(false);
-});
+  test("is hit and is not yet sunk", () => {
+    expect(submarine.hit()).toBe(false);
+  });
 
-test("submarine is hit 2 times and is not yet sunk", () => {
-  const submarine = Ship("submarine");
-  submarine.hit();
+  test("is hit 2 times and is not yet sunk", () => {
+    expect(submarine.hit()).toBe(false);
+  });
 
-  expect(submarine.hit()).toBe(false);
-});
-
-test("submarine is hit 3 times and is sunk", () => {
-  const submarine = Ship("submarine");
-  let i = 0;
-
-  while (i < 2) {
-    submarine.hit();
-    i++;
-  }
-
-  expect(submarine.hit()).toBe(true);
+  test("is hit 3 times and is sunk", () => {
+    expect(submarine.hit()).toBe(true);
+  });
 });
 
 // Cruiser ship
-test("cruiser is hit and is not yet sunk", () => {
+describe("cruiser ship", () => {
   const cruiser = Ship("cruiser");
 
-  expect(cruiser.hit()).toBe(false);
-});
+  test("is hit and is not yet sunk", () => {
+    expect(cruiser.hit()).toBe(false);
+  });
 
-test("cruiser is hit 2 times and is not yet sunk", () => {
-  const cruiser = Ship("cruiser");
-  cruiser.hit();
+  test("is hit 2 times and is not yet sunk", () => {
+    expect(cruiser.hit()).toBe(false);
+  });
 
-  expect(cruiser.hit()).toBe(false);
-});
-
-test("cruiser is hit 3 times and is sunk", () => {
-  const cruiser = Ship("cruiser");
-  let i = 0;
-
-  while (i < 2) {
-    cruiser.hit();
-    i++;
-  }
-
-  expect(cruiser.hit()).toBe(true);
+  test("is hit 3 times and is sunk", () => {
+    expect(cruiser.hit()).toBe(true);
+  });
 });
 
 // Battleship
-test("battleship is hit and is not yet sunk", () => {
+describe("battleship", () => {
   const battleship = Ship("battleship");
 
-  expect(battleship.hit()).toBe(false);
-});
+  test("is hit and is not yet sunk", () => {
+    expect(battleship.hit()).toBe(false);
+  });
 
-test("battleship is hit 3 times and is not yet sunk", () => {
-  const battleship = Ship("battleship");
-  let i = 0;
-
-  while (i < 2) {
+  test("is hit 3 times and is not yet sunk", () => {
     battleship.hit();
-    i++;
-  }
+    expect(battleship.hit()).toBe(false);
+  });
 
-  expect(battleship.hit()).toBe(false);
-});
-
-test("battleship is hit 4 times and is sunk", () => {
-  const battleship = Ship("battleship");
-  let i = 0;
-
-  while (i < 3) {
-    battleship.hit();
-    i++;
-  }
-
-  expect(battleship.hit()).toBe(true);
+  test("is hit 4 times and is sunk", () => {
+    expect(battleship.hit()).toBe(true);
+  });
 });
 
 //Carrier
-test("carrier is hit and is not yet sunk", () => {
+describe("carrier ship", () => {
   const carrier = Ship("carrier");
 
-  expect(carrier.hit()).toBe(false);
-});
+  test("is hit and is not yet sunk", () => {
+    expect(carrier.hit()).toBe(false);
+  });
 
-test("carrier is hit 4 times and is not yet sunk", () => {
-  const carrier = Ship("carrier");
-  let i = 0;
-
-  while (i < 3) {
+  test("is hit 4 times and is not yet sunk", () => {
     carrier.hit();
-    i++;
-  }
-
-  expect(carrier.hit()).toBe(false);
-});
-
-test("ship is hit 5 times and is sunk", () => {
-  const carrier = Ship("carrier");
-  let i = 0;
-
-  while (i < 4) {
     carrier.hit();
-    i++;
-  }
 
-  expect(carrier.hit()).toBe(true);
+    expect(carrier.hit()).toBe(false);
+  });
+
+  test("is hit 5 times and is sunk", () => {
+    expect(carrier.hit()).toBe(true);
+  });
 });
